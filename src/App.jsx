d21b66600e30aaf4104321a9a24f9ca477e37aba
Ccasabianca <107222996+Ccasabianca@users.noreply.github.com>
2025-05-12 11:24:11 +0200

    Activité 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router";
 import Home from "./pages/Home.jsx";
+import Category from "./pages/Category.jsx";
 import Layout from "./components/Layout.jsx";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <Routes>
       <Route element={<Layout />}>
         <Route path="/" element={<Home />} />
+        <Route path="/:category" element={<Category />} />
       </Route>
     </Routes>
   );
