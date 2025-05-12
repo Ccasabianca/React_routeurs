5d7dc65ce4902b36781a54d06eb05728b7a585f0
Ccasabianca <107222996+Ccasabianca@users.noreply.github.com>
2025-05-12 10:19:06 +0200

    Activité 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import { useState } from 'react'
-import './App.css'
+import { Route, Routes } from "react-router";
+import Home from "./pages/Home.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-     
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
