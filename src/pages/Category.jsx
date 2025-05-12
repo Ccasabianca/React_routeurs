f1313afe5cf9faf86e3488126148968d4014954d
Ccasabianca <107222996+Ccasabianca@users.noreply.github.com>
2025-05-12 15:57:43 +0200

    Activité 8

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,11 +1,19 @@
-import { useParams } from "react-router";
+import { useParams, useNavigate } from "react-router";
 
 const Category = () => {
   const { id } = useParams();
+  const navigate = useNavigate();
+
+  const handleGoHome = () => {
+    navigate("/");
+  };
 
   return (
     <div>
       <h1>Catégorie {id}</h1>
+      <button className="btn btn-primary my-2" onClick={handleGoHome}>
+        Retour à la page d'accueil
+      </button>
     </div>
   );
 };
